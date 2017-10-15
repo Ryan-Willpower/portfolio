78358aba36873d9e7082949d9b594c7315e20833
Ryan-Willpower <surasak.c.work@gmail.com>
2017-10-16 02:56:30 +0700

    Refactor and Rebuild somewhere.

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require("express"),
-    app = express()
+const 	express 	= require("express"),
+		sass		= require("node-sass-middleware")
+    	app 		= express()
 
 const index = require("./routes/index")
+
+const setup = {
+	src: './public',
+	dst: './public',
+	indentedSyntax: true,
+	debug: true
+}
+
+app.use(sass(setup))
 
 app.use(express.static('public'))
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express"),
     router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.render('index', {title: 'willpower'})
+    res.render('index', {title: 'willpower', css: '/stylesheet/index.css'})
 })
 
 module.exports = router
